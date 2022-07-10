a5b1579ff4f2a86a5da651a9d67111b4d9161610
Peerapat <85300665+peerapatsiriai@users.noreply.github.com>
2022-07-10 13:14:58 +0700

    Update pagination.js

--- pagination.js
+++ pagination.js
@@ -4,13 +4,11 @@ const condb = require('./condb')
 const Pagination = (page, size) => {
     let max = page * size;
     let min = max - size;
-    // const sql = `SELECT * FROM customer ORDER BY id DESC LIMIT ${min}, ${size}`
     const sql = `SELECT * FROM customer LIMIT ${min}, ${size}`
     condb.connection.query(sql).then(result => {
         console.log(result[0]);
     })
 }
-
 
 Pagination(2,10);
 
